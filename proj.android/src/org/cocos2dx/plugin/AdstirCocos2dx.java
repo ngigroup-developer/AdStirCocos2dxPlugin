1fa3471c0d9299575e430fa3da9d9238112d81fc
reiji-terasaka <reiji-terasaka@united.jp>
2014-01-24 16:24:23 +0900

    package

--- proj.android/src/org/cocos2dx/plugin/AdstirCocos2dx.java
+++ proj.android/src/org/cocos2dx/plugin/AdstirCocos2dx.java
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package com.adstir.cocos2dx;
+package org.cocos2dx.plugin;
 
 import java.util.HashSet;
 import java.util.Hashtable;
